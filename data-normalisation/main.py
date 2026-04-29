bdccc0de244e7e5e154f6a832d20514a4193c036
Steve (Quix) <100689438+SteveRosam@users.noreply.github.com>
2026-04-29 17:02:16 +0100

    Add QuixLab (#3)
    * First version release (#1)
    
    * Initial commit of merged project
    
    * url tweaks
    
    * Add topic config-updates
    
    * Add deployment Data normalisation
    
    * Add topic edf-data
    
    * Add topic normalised-data
    
    * Add topic config-enriched-data
    
    * Connection timeouts and try catch
    
    * Seed manufacturers
    
    * Save file mongodb/app.yaml
    
    * Created from MongoDB
    
    * Add deployment MongoDB
    
    * Remove influx usage
    
    * Update deployment LabTECH
    
    * Save file quix-datalake-timeseries-sink/app.yaml
    
    * Add topic tsbs_data_transformed
    
    * Created from Quix DataLake Timeseries Sink
    
    * Add deployment Quix DataLake Timeseries Sink
    
    * Add deployment Quix DataLake Raw Sink
    
    * small fix
    
    * small fix
    
    * 'test-manager-backend/app.yaml' edited via Quix Portal
    
    * Update deployment Test Manager - Backend
    
    * Update deployment LabTECH
    
    * Update deployment LabTECH
    
    * seeding?
    
    * fix product category selection
    
    * Show seeded options in labview sim for ease of use
    
    * Edited 'main.py'
    
    * Update deployment LabTECH
    
    * Update deployment LabTECH
    
    * Update deployment Dynamic Configuration Manager
    
    * port and url fixes
    
    * try this url
    
    * hardcode cfg service url
    
    * try again
    
    * Edited 'quix.yaml'
    
    * Save file marimo/app.yaml
    
    * Created from Marimo
    
    * Add deployment Marimo
    
    * Update file marimo/main.py
    
    * add marimo
    
    * add anomolies
    
    * Update deployment LabTECH
    
    * Update deployment Rig ECU
    
    * Update deployment Test Manager - Backend
    
    * Update deployment Marimo
    
    * Update deployment Test Manager - Frontend
    
    * Update deployment MongoDB
    
    * Update deployment test-rig-data
    
    * Update deployment Dynamic Configuration Manager
    
    * Update deployment Data normalisation
    
    * Update deployment Config Enricher
    
    * Update deployment Quix DataLake Raw Sink
    
    * Update deployment Quix DataLake Timeseries Sink
    
    ---------
    
    Co-authored-by: ServiceAccount user <serviceacc+quixers@quix.io>
    Co-authored-by: Steve Rosam <steve+quixers@quix.io>
    
    * Fixes (#2)
    
    * Update deployment Quix DataLake Timeseries Sink
    
    * Update file marimo/main.py
    
    * Edited 'quix.yaml'
    
    * Save file postgresql/app.yaml
    
    * Created from postgreSQL
    
    * Edited 'quix.yaml'
    
    * Update deployment Quix TS Datalake API
    
    * Edited 'quix.yaml'
    
    ---------
    
    Co-authored-by: Tomas Neubauer <tomas+quixers@quix.io>
    Co-authored-by: Steve Rosam <steve+quixers@quix.io>
    
    * Edited 'quix.yaml'
    
    * Edited 'quix.yaml'
    
    * Edited 'quix.yaml'
    
    * Update deployment Quix TS Datalake API
    
    * Edited 'quix.yaml'
    
    * Update deployment Quix DataLake Timeseries Sink
    
    * Update deployment Test Manager - Frontend
    
    * Update deployment Marimo
    
    * Update deployment Quix TS Query UI
    
    * Update deployment Quix DataLake Timeseries Sink
    
    * Update file marimo/main.py
    
    * Update file marimo/main.py
    
    * Update file marimo/main.py
    
    * Edited 'quix.yaml'
    
    * Update file marimo/main.py
    
    * Update file marimo/main.py
    
    * Update deployment Quix TS Query UI
    
    * Update deployment LabTECH
    
    * Save file quixlab/app.yaml
    
    * Created from QuixLab
    
    * Add deployment QuixLab
    
    * Edited 'dockerfile'
    
    * Update file quixlab/main.py
    
    * Edited 'dockerfile'
    
    * Update file quixlab/main.py
    
    ---------
    
    Co-authored-by: ServiceAccount user <serviceacc+quixers@quix.io>
    Co-authored-by: Steve Rosam <steve+quixers@quix.io>
    Co-authored-by: Tomas Neubauer <tomas+quixers@quix.io>
    Co-authored-by: Steve Quix <steve+demo-env@quix.io>
    Co-authored-by: Tomas Quix <tomas+new-demo@quix.io>

--- data-normalisation/main.py
+++ data-normalisation/main.py
@@ -1,6 +1,7 @@
 # import the Quix Streams modules for interacting with Kafka.
 # For general info, see https://quix.io/docs/quix-streams/introduction.html
-from quixstreams import Application
+from quixstreams import Application, State
+import time
 import os
 
 # for local dev, load env vars from a .env file
