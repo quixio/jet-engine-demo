7f43662751263f9ef01a726b2b2f30e27136a6e3
Tomas Quix <tomas+new-demo@quix.io>
2026-04-29 15:26:57 +0000

    Update file quixlab/main.py

--- quixlab/main.py
+++ quixlab/main.py
@@ -9,7 +9,8 @@ def config_enriched_data(experiment):
     FROM config_enriched_data
     WHERE campaign_id = '{experiment[1]}'
       AND environment_id = '{experiment[2]}'
-      AND test_id = '{experiment[3]}'""")
+      AND test_id = '{experiment[3]}'
+    ORDER BY timestamp""")
 
 
 @canvas.cell(position=(-255, 362), size=(724, 466), code_height=0)
@@ -43,6 +44,14 @@ def experiment():
     """)
 
     [heading, campaign_id, environment_id, test_id]
+
+
+@canvas.notebook(position=(1687, 185), size=(959, 702), code_height=200, viz={'cells': {'0': {'type': 'table', 'x': 'timestamp', 'y': 'ina260__voltage_v'}, '1': {'type': 'line', 'x': 'timestamp', 'y': 'ina260__voltage_v'}}, 'outputCell': 1, 'outputCells': [1, 2], 'type': 'line', 'x': 'timestamp', 'y': ['timestamp_diff']})
+def cell_2(config_enriched_data):
+    # %%
+    config_enriched_data["timestamp_diff"] = config_enriched_data["timestamp"].diff()
+    # %%
+    config_enriched_data
 
 
 if __name__ == "__main__":
